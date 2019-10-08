a0cc7ad5a43eb60e0e3ebf2296a2daba07cb729d
soheila <soheilalotfi@ymail.com>
2019-10-08 14:37:27 -0400

    add className to some elements

--- src/components/SearchResultContainer.js
+++ src/components/SearchResultContainer.js
@@ -29,12 +29,13 @@ class SearchResultConatiner extends Component {
     render() {
         return (
             <>
-                <input type="text" name="search" value={this.state.search} onChange={this.handleInputChange}></input>
-                <button type="submit" onClick={this.handleSubmitForm}>Submit</button>
-                <ul>
+                <lable  className="ml-5 mt-5"htmlFor="search">Search Giphy:</lable><br/>
+                <input className="ml-5 " type="text" name="search" value={this.state.search} onChange={this.handleInputChange}></input> {" "} <br />
+                <button className="btn btn-primary mt-3 ml-5" type="submit" onClick={this.handleSubmitForm}>Submit</button>
+                <ul className="list-group">
                     {this.state.results.map(result => (
 
-                        <li key={result.id}><img alt={result.title} src={result.images.original.url}></img></li>
+                        <li className="list-group-item" key={result.id}><img alt={result.title} src={result.images.original.url}></img></li>
                     ))}
                 </ul>
             </>
