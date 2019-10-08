308720eba723d4639d58547370b92dcfae93b103
soheila <soheilalotfi@ymail.com>
2019-10-08 14:06:33 -0400

    add handleInputchange method

--- src/components/SearchResultContainer.js
+++ src/components/SearchResultContainer.js
@@ -1,17 +1,27 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class SearchResultConatiner extends Component {
 
-    state={
-        search :"",
+    state = {
+        search: "",
         results: []
     }
-    render(){
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    render() {
         return (
             <>
-            <input type="text" name="search" value={this.state.search} onChange={this.handleInputChange}></input>
-            <button type="submit" onClick={this.handleSubmitForm}>Submit</button>
+                <input type="text" name="search" value={this.state.search} onChange={this.handleInputChange}></input>
+                <button type="submit" onClick={this.handleSubmitForm}>Submit</button>
             </>
         )
     }
 }
+
+export default SearchResultConatiner;
